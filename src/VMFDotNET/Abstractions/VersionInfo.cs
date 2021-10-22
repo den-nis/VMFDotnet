84ee13bfb230b12f3ce6901c628cc0ee8afd5801
Dennis <50838791+den-nis@users.noreply.github.com>
2021-10-22 22:33:52 +0200

    Added abstractions

---
+++ src/VMFDotNET/Abstractions/VersionInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMFDotNET.Linq;
+
+namespace VMFDotNET.Abstractions
+{
+	public class VersionInfo
+	{
+		public int EditorVersion { get; set; }
+		public int EditorBuild { get; set; }
+		public int MapVersion { get; set; }
+		public int FormatVersion { get; set; } = 100;
+		public int Prefab { get; set; }
+	}
+}
---
+++ src/VMFDotNET/Abstractions/ViewSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMFDotNET.Abstractions
+{
+	public class ViewSettings
+	{
+		public bool SnapToGrid { get; set; }
+		public bool ShowGrid { get; set; }
+		public bool ShowLogicalGrid { get; set; }
+		public int  GridSpacing { get; set; }
+		public bool Show3DGrid { get; set; }
+	}
+}
